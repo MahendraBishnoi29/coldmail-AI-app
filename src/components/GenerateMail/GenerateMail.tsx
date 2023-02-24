f96904388320281e920e4bcd97424b76ea6e2bb5
Mahendra <mahendrabrowny@gmail.com>
2023-02-24 09:58:10 +0530

    animate result with framer-motion

--- src/components/GenerateMail/GenerateMail.tsx
+++ src/components/GenerateMail/GenerateMail.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { Toaster, toast } from "react-hot-toast";
+import { motion, AnimatePresence } from "framer-motion";
 
 type Props = {};
 
@@ -60,25 +61,30 @@ const GenerateMail = (props: Props) => {
       </h2>
 
       <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-        <div
-          className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-cell border"
-          onClick={() => {
-            toast("Email copied to clipboard", {
-              icon: "📋",
-              style: {
-                borderRadius: "10px",
-                background: "#333",
-                color: "#fff",
-              },
-            });
-          }}
-        >
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-            praesentium dolore reiciendis dignissimos odio rem nam tempora ab
-            beatae doloribus.
-          </p>
-        </div>
+        <AnimatePresence mode="wait">
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ delay: 1, duration: 0.5, ease: "easeOut" }}
+            className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-cell border"
+            onClick={() => {
+              toast("Email copied to clipboard", {
+                icon: "📋",
+                style: {
+                  borderRadius: "10px",
+                  background: "#333",
+                  color: "#fff",
+                },
+              });
+            }}
+          >
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              praesentium dolore reiciendis dignissimos odio rem nam tempora ab
+              beatae doloribus.
+            </p>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );
