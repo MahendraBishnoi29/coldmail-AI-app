27a1c1398f4fd5aed40e969b3a0a2892f401891a
Mahendra <mahendrabrowny@gmail.com>
2023-05-12 19:31:11 +0530

    add vercel analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from "next/head";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import Navbar from "@/components/Navbar/Navbar";
-import Header from "@/components/Header/Header";
-import GenerateMail from "@/components/GenerateMail/GenerateMail";
 import Footer from "@/components/Footer/Footer";
+import GenerateMail from "@/components/GenerateMail/GenerateMail";
+import Header from "@/components/Header/Header";
+import Navbar from "@/components/Navbar/Navbar";
+import { Inter } from "@next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
